8a239ca52bc7ad4b2178f2d73b1af76552a1ce30
Gmright2 <67715829+bitokenja3@users.noreply.github.com>
2021-10-11 18:04:24 -0500

    Update and rename Gmright2.md to Gmright2.go

--- Gmright2.go
+++ Gmright2.go
@@ -8,3 +8,4 @@
 gmright2-Coller/gmright2-Coller is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
 You can click the Preview link to take a look at your changes.
 --->
+# 143ecaa2f413aa4d4a6e8e6ad21bb9f60beb3217
